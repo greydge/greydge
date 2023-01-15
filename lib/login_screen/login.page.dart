84492011e340f7c74be03ce19603120b5ff77726
pulkitappentus <65279696+pulkit0212@users.noreply.github.com>
2023-01-15 11:25:57 +0530

    splash and login

---
+++ lib/login_screen/login.page.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  State<LoginPage> createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            //logo(160, 160),
+            Text('Login')
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/splash_screen/splash.page.dart
@@ -0,0 +1,74 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import '../login_screen/login.page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+      const Duration(seconds: 3),
+          () => Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const LoginPage(),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            //logo(160, 160),
+            const SizedBox(
+              height: 25,
+            ),
+            richText(30),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget richText(double fontSize) {
+    return Text.rich(
+      TextSpan(
+        children: const [
+          TextSpan(
+            text: 'LOGIN ',
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          TextSpan(
+            text: 'PAGES \nUI ',
+            style: TextStyle(
+              color: Color(0xFFFE9879),
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+          TextSpan(
+            text: 'KIT',
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+            ),
+          ),
+        ],
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
